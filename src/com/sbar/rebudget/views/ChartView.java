73a037470910d425ad68ad0ccce9773dcaf8678b
Alexander Lopatin <sbar.geek@gmail.com>
2012-11-24 14:49:41 +0400

    ui update

--- src/com/sbar/rebudget/views/ChartView.java
+++ src/com/sbar/rebudget/views/ChartView.java
@@ -31,38 +31,51 @@ public class ChartView extends View {
     }
 
     float m_lastAngle;
+    float m_rectW;
+    float m_textY;
 
     @Override
     protected void onDraw(Canvas canvas) {
         m_lastAngle = 0.0f;
+        m_textY = 0.0f;
+        m_rectW = getWidth() > getHeight() ? getHeight() : getWidth();
+
         for (Piece p : m_pieces)
             drawPiece(canvas, p);
     }
 
     private void drawPiece(Canvas canvas, final Piece piece) {
         m_paint.setStyle(Paint.Style.FILL);
+        m_paint.setTextSize(19);
 
-        float rectW = getWidth() > getHeight() ? getHeight() : getWidth();
+        float padding = m_paint.getFontSpacing();
+
+        m_paint.setColor(piece.color);
+        canvas.drawText(piece.text0, m_rectW + padding, m_textY + padding, m_paint);
+        m_textY += padding;
+
+        // FIXME: we should check money instead of angles
+        int moneyColor = piece.anglePlanned > piece.angleSpent
+                         ? 0xffffffff : 0xffff0000;
+        m_paint.setColor(moneyColor);
+        canvas.drawText(piece.text1, m_rectW + padding, m_textY + padding, m_paint);
+        m_textY += padding * 3.0f;
+
+        m_paint.setColor(piece.color);
+        canvas.drawArc(new RectF(0, 0, m_rectW, m_rectW),
+                       m_lastAngle,
+                       piece.anglePlanned,
+                       true,
+                       m_paint);
 
         m_paint.setColor(getDarkerColor(piece.color, 0x80));
-        canvas.drawArc(new RectF(0, 0, rectW, rectW),
+        canvas.drawArc(new RectF(5, 5, m_rectW - 5, m_rectW - 5),
                        m_lastAngle,
                        piece.angleSpent,
                        true,
                        m_paint);
-        m_lastAngle += piece.angleSpent;
 
-        // remaining
-        if (piece.anglePlanned - piece.angleSpent <= 0)
-            return;
-
-        m_paint.setColor(piece.color);
-        canvas.drawArc(new RectF(0, 0, rectW, rectW),
-                       m_lastAngle,
-                       piece.anglePlanned - piece.angleSpent,
-                       true,
-                       m_paint);
-        m_lastAngle += (piece.anglePlanned - piece.angleSpent);
+        m_lastAngle += piece.anglePlanned;
     }
 
     int getDarkerColor(int color, int darker) {
@@ -91,7 +104,15 @@ public class ChartView extends View {
         float angleSpent = (moneySpent * 360.0f) / m_totalMoney;
         if (angleSpent > anglePlanned)
             angleSpent = anglePlanned;
-        m_pieces.add(new Piece(text, color, anglePlanned, angleSpent));
+
+        // TODO: number bits formatting
+        String spentText = String.format(
+            "Spent %.2f of %.2f",
+            moneySpent,
+            moneyPlanned
+        );
+
+        m_pieces.add(new Piece(text, spentText, color, anglePlanned, angleSpent));
     }
 
     void sortPieces() {
@@ -99,14 +120,16 @@ public class ChartView extends View {
     }
 
     class Piece {
-        public Piece(final String text_, int color_,
-                     float anglePlanned_, float angleSpent_) {
-            text = text_;
+        public Piece(final String text0_, final String text1_,
+                     int color_, float anglePlanned_, float angleSpent_) {
+            text0 = text0_;
+            text1 = text1_;
             color = color_;
             anglePlanned = anglePlanned_;
             angleSpent = angleSpent_;
         }
-        public String text;
+        public String text0;
+        public String text1;
         public int color;
         public float anglePlanned;
         public float angleSpent;
